42f95ae2da9ac23409c263136b6e2b0ef100e015
BiancaBlas <84021462+BiancaBlas@users.noreply.github.com>
2021-05-27 16:03:02 +0200

    Add files via upload

---
+++ js_programma_op_pc/script.js
@@ -0,0 +1,6 @@
+console.log("Hallo Winc Academy studenten")
+//dit is een grote som
+/*let vermenigvuldiging = 1230941 * 1823794
+console.log(vermenigvuldiging)
+let deling = 637263 / 54
+console.log(deling)*/
