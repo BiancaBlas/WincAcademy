87c6fb2b126ef90e8f0a60ff5340f15f96a0f1cb
BiancaBlas <84021462+BiancaBlas@users.noreply.github.com>
2021-05-27 10:12:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hello Winc Academy")
+let name = 'Bianca'
+console.log(name)
+console.log(4+4)
+console.log('4'+'4')
+//geen if als name, je mag geen woorden gebruiken die al toegewezen zijn in js
+let age = "36"
+console.log(typeof age)
