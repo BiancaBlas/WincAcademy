7a35430813c86c4bf71c19301a85203402459256
BiancaBlas <84021462+BiancaBlas@users.noreply.github.com>
2021-05-28 15:29:32 +0200

    Add files via upload

---
+++ objects_arrays/script.js
@@ -0,0 +1,22 @@
+let person = {
+    name: "Bianca",
+    age: 36,
+    evaluations: [7, 10, 9]
+};
+let colors =[ "groen", "blauw", "rood"
+]
+colors.push("geel");
+colors.push(5);
+let object= {greeting: "Hi ik ben een object"}
+colors.push(object)
+let last = colors.length -1 ;
+/* console.log(person);
+console.log(person.name);
+console.log(person["age"]);
+console.log(person.evaluations);
+console.log(typeof person.evaluations);
+console.log(colors);
+console.log(colors.length);
+console.log(colors[0]); 
+console.log(colors[last]);*/
+console.log(colors[5]);
