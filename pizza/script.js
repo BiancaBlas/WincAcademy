83c67bad5e7fb09efbbad6cd1ecbe18ad4baa767
BiancaBlas <84021462+BiancaBlas@users.noreply.github.com>
2021-05-29 11:11:24 +0200

    Add files via upload

---
+++ pizza/script.js
@@ -0,0 +1,23 @@
+function makePizza() {
+    console.log("zet de oven aan op 180 graden");
+    console.log("pak een pizzabodem");
+    console.log("doe tomatenpuree op de pizzabodem");
+    console.log("doe kruiden op de pizzabodem");
+    console.log("doe fruit op de pizzabodem");
+    console.log("doe mozzerella op de pizzabodem");
+    console.log("doe de pizza in de oven voor 14 minuten");
+}
+
+
+function makeSushi() {
+    console.log("kook rijst");
+    console.log("snijd komkommer");
+    console.log("leg een laagje rijst op een strook zeewier");
+    console.log("leg een stukje komkommer op het laagje rijst");
+    console.log("rol het geheel op");
+}
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
